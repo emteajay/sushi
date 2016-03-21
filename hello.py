44fa236327888d47b7844c2108e92fade8315a1d
Michael <mtjjoyce@gmail.com>
2016-03-21 17:55:07 -0400

    added style sheets

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
 app.config.from_object(__name__)
 
 @app.route("/")
@@ -9,5 +10,35 @@ def main():
 	return render_template('main.html')
 
 
+
+
+
+# start the development server using the run() method
 if __name__== "__main__":
 	app.run(debug=True)
+
+# @app.route("/test/<search_query>")
+# def search(search_query):
+# 	return search_query
+
+# @app.route("/integer/<int:value>")
+# def int_type(value):
+# 	print value + 1
+# 	return "correct!"
+
+# @app.route("/float/<float:value>")
+# def float_type(value):
+# 	print value + 1
+# 	return "correct!"
+
+# @app.route("/path/<path:value>")
+# def path_type(value):
+# 	print value
+# 	return "correct!"	
+
+# @app.route("/name/<name>")
+# def index(name):
+# 	if name.lower() == "michael":
+# 		return "Hello, {}".format(name), 200
+# 	else:
+# 		return "Not Found", 404	
